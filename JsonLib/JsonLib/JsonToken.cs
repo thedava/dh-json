645469a0c8daf8446f794f6582260e9acd90af0d
David S. <thedava@users.noreply.github.com>
2014-12-20 13:21:15 +0000

    - Added ToJson-Method to JsonResult classes - Added more docs

--- JsonLib/JsonLib/JsonToken.cs
+++ JsonLib/JsonLib/JsonToken.cs
@@ -4,16 +4,63 @@ using System.Text;
 
 internal enum JsonToken : int
 {
+    /// <summary>
+    /// No Token
+    /// </summary>
     None = 0,
-    CurlyOpen = 1,      // {
-    CurlyClose = 2,     // }
-    SquaredOpen = 3,    // [
-    SquaredClose = 4,   // ]
-    Colon = 5,          // :
-    Comma = 6,          // ,
-    String = 7,         // ""
-    Number = 8,         // 123.45
-    BoolTrue = 9,       // true
-    BoolFalse = 10,     // false
-    Null = 11           // null
+
+    /// <summary>
+    /// Open curly bracket -> {
+    /// </summary>
+    CurlyOpen = 1,
+
+    /// <summary>
+    /// Closed curly bracket -> }
+    /// </summary>
+    CurlyClose = 2,
+
+    /// <summary>
+    /// Open squared bracket -> [
+    /// </summary>
+    SquaredOpen = 3,
+
+    /// <summary>
+    /// Closed squared bracket -> ]
+    /// </summary>
+    SquaredClose = 4,
+
+    /// <summary>
+    /// A colon -> :
+    /// </summary>
+    Colon = 5,
+
+    /// <summary>
+    /// A comma -> ,
+    /// </summary>
+    Comma = 6,
+
+    /// <summary>
+    /// A string -> ""
+    /// </summary>
+    String = 7,
+
+    /// <summary>
+    /// A number (float or int) -> 123.45
+    /// </summary>
+    Number = 8,
+
+    /// <summary>
+    /// A boolean true -> true
+    /// </summary>
+    BoolTrue = 9,
+
+    /// <summary>
+    /// A boolean false -> false
+    /// </summary>
+    BoolFalse = 10,
+
+    /// <summary>
+    /// An empty value -> null
+    /// </summary>
+    Null = 11
 }
