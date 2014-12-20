6d3506135a70700ed0a37f36a1ea42e2c1b93788
TehDava <TehDava@7ee3e1df-a05e-650f-bf52-709725f104f8>
2014-12-20 13:21:15 +0000

    - Added ToJson-Method to JsonResult classes - Added more docs

--- trunk/JsonLib/JsonLib/Caster.cs
+++ trunk/JsonLib/JsonLib/Caster.cs
@@ -2,8 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
+/// <summary>
+/// This class is for converting values into other types
+/// </summary>
 internal class Caster
 {
+    /// <summary>
+    /// Creates a caster exception (if strict mode is enabled)
+    /// </summary>
+    /// <param name="field">The field that is invalid</param>
+    /// <param name="dataType">The expected data type</param>
+    /// <param name="error">The original cast exception</param>
     private static void throwError(object field, string dataType, Exception error = null)
     {
         string def = (field is Int32) ? "index" : "field";
@@ -12,6 +21,12 @@ internal class Caster
         throw (error == null) ? new InvalidCastException(msg) : new InvalidCastException(msg, error);
     }
 
+    /// <summary>
+    /// Tries to parse a value as int
+    /// </summary>
+    /// <param name="value">The value that should be converted</param>
+    /// <param name="field">The original index of this element</param>
+    /// <returns>A nullable integer</returns>
     public static int? ParseInt(object value, object field)
     {
         int? result = null;
@@ -22,16 +37,29 @@ internal class Caster
         catch (Exception error)
         {
             if (Json.STRICT && !result.HasValue)
+            {
                 throwError(field, "Int32", error);
+            }
         }
         return result;
     }
 
+    /// <summary>
+    /// Tries to parse a value as string
+    /// </summary>
+    /// <param name="value">The value that should be converted</param>
+    /// <returns>A string (or null)</returns>
     public static string ParseString(object value)
     {
         return value.ToString();
     }
 
+    /// <summary>
+    /// Tries to parse a value as double
+    /// </summary>
+    /// <param name="value">The value that should be converted</param>
+    /// <param name="field">The original index of this element</param>
+    /// <returns>A nullable double</returns>
     public static double? ParseDouble(object value, object field)
     {
         double? result = null;
@@ -42,11 +70,19 @@ internal class Caster
         catch (Exception error)
         {
             if (Json.STRICT && !result.HasValue)
+            {
                 throwError(field, "Double", error);
+            }
         }
         return result;
     }
 
+    /// <summary>
+    /// Tries to parse a value as bool
+    /// </summary>
+    /// <param name="value">The value that should be converted</param>
+    /// <param name="field">The original index of this element</param>
+    /// <returns>A nullable boolean</returns>
     public static bool? ParseBool(object value, object field)
     {
         bool? result = null;
@@ -57,27 +93,45 @@ internal class Caster
         catch (Exception error)
         {
             if (Json.STRICT && !result.HasValue)
+            {
                 throwError(field, "Boolean", error);
+            }
         }
         return result;
     }
 
+    /// <summary>
+    /// Tries to parse a value as json object
+    /// </summary>
+    /// <param name="value">The value that should be converted</param>
+    /// <param name="field">The original index of this element</param>
+    /// <returns>A JsonObject (or null)</returns>
     public static JsonObject ParseObject(object value, object field)
     {
         JsonObject result = (value as JsonObject);
 
         if (Json.STRICT && result == null)
+        {
             throwError(field, "JsonObject");
+        }
 
         return result;
     }
 
+    /// <summary>
+    /// Tries to parse a value as json array
+    /// </summary>
+    /// <param name="value">The value that should be converted</param>
+    /// <param name="field">The original index of this element</param>
+    /// <returns>A JsonArray (or null)</returns>
     public static JsonArray ParseArray(object value, object field)
     {
         JsonArray result = (value as JsonArray);
 
         if (Json.STRICT && result == null)
+        {
             throwError(field, "JsonArray");
+        }
 
         return result;
     }
